da84ce7181d466a0208346860754dc2938526c99
Rody Gosset <rody.gosset@gmail.com>
2026-01-15 12:50:59 +0100

    initial package structure

---
+++ src/options.ts
@@ -0,0 +1,126 @@
+import type { Prettify } from "./utils"
+import type { QueryKey, UseMutationOptions, UseQueryOptions } from "@tanstack/react-query"
+import { mutationOptions as _mutationOptions, queryOptions as _queryOptions } from "@tanstack/react-query"
+import { Cause, Effect, Schema, Exit, ManagedRuntime } from "effect"
+
+export interface QueryOptions<
+	TData = unknown,
+	TError = never,
+	TQueryFnData = TData,
+	TQueryKey extends QueryKey = QueryKey,
+	R = never
+> extends Prettify<
+		Omit<UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>, "queryFn"> & {
+			queryFn: () => Effect.Effect<TData, TError, R>
+			schema?: Schema.Schema<TData, TQueryFnData>
+			consumeAbortSignal?: boolean
+		}
+	> {}
+
+const makeQueryOptions =
+	<R>() =>
+	<TQueryFnData = unknown, TError = never, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(
+		options: QueryOptions<TQueryFnData, TError, TData, TQueryKey, R>
+	) =>
+		options
+
+export interface MutationOptions<
+	TData = unknown,
+	TError = never,
+	TVariables = void,
+	TOnMutateResult = unknown,
+	R = never
+> extends Prettify<
+		Omit<UseMutationOptions<TData, Cause.Cause<TError>, TVariables, TOnMutateResult>, "mutationFn"> & {
+			mutationFn: (variables: TVariables) => Effect.Effect<TData, TError, R>
+		}
+	> {}
+
+const makeMutationOptions =
+	<R>() =>
+	<TData = unknown, TError = never, TVariables = void, TOnMutateResult = unknown>(
+		options: MutationOptions<TData, TError, TVariables, TOnMutateResult, R>
+	) =>
+		options
+
+export const makeOptions = <R>() => ({
+	queryOptions: makeQueryOptions<R>(),
+	mutationOptions: makeMutationOptions<R>(),
+})
+
+const runAbortablePromiseExit =
+	<A, E, R, RE>(runtime: ManagedRuntime.ManagedRuntime<R, RE>, signal?: AbortSignal) =>
+	(effect: Effect.Effect<A, E, R>) =>
+		runtime.runPromiseExit(effect, { signal })
+
+export function toQueryOptions<
+	TData = unknown,
+	TError = never,
+	TQueryFnData = TData,
+	TQueryKey extends QueryKey = QueryKey,
+	R = never,
+	RE = never
+>(
+	options: QueryOptions<TData, TError, TQueryFnData, TQueryKey, R>,
+	runtime: ManagedRuntime.ManagedRuntime<R, RE>,
+	suspense: boolean = false
+): UseQueryOptions<TQueryFnData, Cause.Cause<TError>, TData, TQueryKey> {
+	return _queryOptions({
+		...options,
+		queryFn: (ctx) =>
+			options
+				.queryFn()
+				.pipe(
+					Effect.withSpan(suspense ? "useSuspenseQuery" : "useQuery", {
+						attributes: { queryKey: options.queryKey },
+					}),
+					!!options.consumeAbortSignal ? runAbortablePromiseExit(runtime, ctx.signal) : runtime.runPromiseExit
+				)
+				.then(
+					Exit.match({
+						onSuccess: (data): TQueryFnData =>
+							options.schema
+								? Schema.encodeSync(options.schema)(data)
+								: (data as unknown as TQueryFnData),
+						onFailure: (cause): TQueryFnData => {
+							throw cause
+						},
+					})
+				),
+		select: (data) => (options.schema ? Schema.decodeSync(options.schema)(data) : data),
+	}) as UseQueryOptions<TQueryFnData, Cause.Cause<TError>, TData, TQueryKey>
+}
+
+export function toMutationOptions<
+	TData = unknown,
+	TError = never,
+	TVariables = void,
+	TOnMutateResult = unknown,
+	R = never,
+	RE = never
+>(
+	options: MutationOptions<TData, TError, TVariables, TOnMutateResult, R>,
+	runtime: ManagedRuntime.ManagedRuntime<R, RE>
+): UseMutationOptions<TData, Cause.Cause<TError>, TVariables, TOnMutateResult> {
+	return _mutationOptions({
+		...options,
+		mutationFn: (vars) =>
+			options
+				.mutationFn(vars)
+				.pipe(
+					Effect.withSpan("useMutation", {
+						attributes: { mutationKey: options.mutationKey },
+					}),
+					runtime.runPromiseExit
+				)
+				.then(
+					(exit) =>
+						Exit.match(exit, {
+							onSuccess: (data) => data,
+							onFailure(cause) {
+								throw cause
+							},
+						}) as TData
+				),
+	}) as UseMutationOptions<TData, Cause.Cause<TError>, TVariables, TOnMutateResult>
+}
---
+++ src/runtime.tsx
@@ -0,0 +1,118 @@
+"use client"
+
+import {
+	useMutation as _useMutation,
+	useQuery as _useQuery,
+	useSuspenseQuery as _useSuspenseQuery,
+	type QueryKey,
+	type UseMutationResult,
+	type UseQueryResult,
+	type UseSuspenseQueryOptions,
+	type UseSuspenseQueryResult,
+} from "@tanstack/react-query"
+import { Cause, Effect, Either, Layer, ManagedRuntime } from "effect"
+import React, { useEffect, useMemo, useRef } from "react"
+import type { MutationOptions, QueryOptions } from "./options"
+import { toMutationOptions, toQueryOptions } from "./options"
+
+const memoMap = Effect.runSync(Layer.makeMemoMap)
+
+export function make<R, E = never>(layer: Layer.Layer<R, E>) {
+	const RuntimeContext = React.createContext<ManagedRuntime.ManagedRuntime<R, E> | null>(null)
+
+	function useRuntime() {
+		const runtime = React.useContext(RuntimeContext)
+
+		if (!runtime) throw new Error("useRuntime must be used within a RuntimeProvider")
+
+		return runtime
+	}
+
+	function useQuery<TData = unknown, TError = never, TQueryFnData = TData, TQueryKey extends QueryKey = QueryKey>(
+		options: QueryOptions<TData, TError, TQueryFnData, TQueryKey, R>
+	): UseQueryResult<TData, Cause.Cause<TError>> {
+		const runtime = useRuntime()
+
+		const queryOptions = useMemo(() => toQueryOptions(options, runtime), [options, runtime])
+
+		return _useQuery(queryOptions) as UseQueryResult<TData, Cause.Cause<TError>>
+	}
+
+	function useSuspenseQuery<
+		TData = unknown,
+		TError = unknown,
+		TQueryFnData = TData,
+		TQueryKey extends QueryKey = QueryKey
+	>(
+		options: QueryOptions<TData, TError, TQueryFnData, TQueryKey, R>
+	): Either.Either<UseSuspenseQueryResult<TData, Cause.Cause<TError>>, Cause.Cause<TError>> {
+		const runtime = useRuntime()
+
+		const queryOptions = useMemo(
+			() =>
+				toQueryOptions(options, runtime, true) as UseSuspenseQueryOptions<
+					TQueryFnData,
+					Cause.Cause<TError>,
+					TData,
+					TQueryKey
+				>,
+			[options, runtime]
+		)
+
+		try {
+			// eslint-disable-next-line react-compiler/react-compiler
+			const query = _useSuspenseQuery<TQueryFnData, Cause.Cause<TError>, TData, TQueryKey>(
+				queryOptions
+			) as UseSuspenseQueryResult<TData, Cause.Cause<TError>>
+			return useMemo(() => Either.right(query), [query])
+		} catch (e) {
+			if (e instanceof Promise) throw e
+
+			return useMemo(() => Either.left((Cause.isCause(e) ? e : Cause.die(e)) as Cause.Cause<TError>), [e])
+		}
+	}
+
+	function useMutation<TData = unknown, TError = never, TVariables = void, TOnMutateResult = unknown>(
+		options: MutationOptions<TData, TError, TVariables, TOnMutateResult, R>
+	): UseMutationResult<TData, Cause.Cause<TError>, TVariables, TOnMutateResult> {
+		const runtime = useRuntime()
+
+		const mutationOptions = useMemo(() => toMutationOptions(options, runtime), [options, runtime])
+
+		return _useMutation(mutationOptions) as UseMutationResult<
+			TData,
+			Cause.Cause<TError>,
+			TVariables,
+			TOnMutateResult
+		>
+	}
+
+	function RuntimeProvider(props: { children: React.ReactNode }) {
+		const runtime = useMemo(() => ManagedRuntime.make(layer, memoMap), [])
+
+		const mountedRef = useRef(false)
+
+		useEffect(() => {
+			if (!mountedRef.current) {
+				mountedRef.current = true
+				return
+			}
+
+			return () => {
+				runtime.dispose()
+			}
+		}, [runtime])
+
+		return <RuntimeContext.Provider value={runtime}>{props.children}</RuntimeContext.Provider>
+	}
+
+	return {
+		useRuntime,
+		RuntimeProvider,
+		toQueryOptions,
+		useQuery,
+		useSuspenseQuery,
+		toMutationOptions,
+		useMutation,
+	} as const
+}
